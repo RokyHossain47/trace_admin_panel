6ea9e593cfb080579f698ca93930b890c477677e
rokyhossain47 <mdrokey2000@gmail.com>
2026-04-19 00:25:57 +0600

    [new] setup back4app

--- Configs.php
+++ Configs.php
@@ -11,9 +11,9 @@ session_start();
 
 try {
     ParseClient::initialize(
-        '',/*APP ID*/
-        '', /*REST API key*/
-        ''/*MASTER key*/
+        'yIAEelcOnl3YnRYp9Xft6fAfI6CJLU0TLtKYf0nP',/*APP ID*/
+        '2u2DEllH51wXLwDElQggSx7y7vJu3X1OgTn2ELIM', /*REST API key*/
+        'AsDVQmszF2ybh9MeeYxW6tsWdfmJbCnxwUrlkkGt'/*MASTER key*/
     );
     ParseClient::setServerURL('https://parseapi.back4app.com/','/'); // For back4app use: https://parseapi.back4app.com/'
     ParseClient::setStorage( new ParseSessionStorage());
