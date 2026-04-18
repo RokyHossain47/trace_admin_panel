ad3a6d07144130cff7a9263d1649340bd32efc74
rokyhossain47 <mdrokey2000@gmail.com>
2026-04-19 00:09:08 +0600

    [init]

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'parse/php-sdk',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9868dbcbda8ac7fc6764f8bf460a9a9a179d8d32',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'parse/php-sdk' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9868dbcbda8ac7fc6764f8bf460a9a9a179d8d32',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
